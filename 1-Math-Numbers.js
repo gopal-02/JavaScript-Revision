4af1dd3225fe0559e57cf021911bb0e4bba2144d
gopal-02 <gopalkumar8220@gmail.com>
2025-03-12 00:11:15 +0530

    Add new methords

--- 1-Math-Numbers.js
+++ 1-Math-Numbers.js
@@ -4,3 +4,12 @@ let id = new Number(200)
 console.log(typeof a)
 console.log(id)
 console.log(typeof id)
+
+let id2 = new Number(200000000)
+
+console.log(id2.toLocaleString('en-IN'))
+console.log(id2.toString)
+
+let age = 20.2544
+
+console.log(age.toFixed(1))
